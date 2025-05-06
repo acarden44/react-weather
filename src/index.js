e580cb68608b13803c4a075df39ca601d1db67c9
acarden44 <aprilcarden44@gmail.com>
2025-05-06 14:52:48 -0400

    Updating App

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
