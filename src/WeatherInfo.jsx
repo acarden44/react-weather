df5117b8e585a634cbcea8fba4e2c55fcd2e515e
acarden44 <aprilcarden44@gmail.com>
2025-05-07 07:50:58 -0400

    Small tweaks

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -1,35 +1,39 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import "./App.css";
 
 export default function WeatherInfo({ data, unit }) {
   return (
-    <>
-      <div className="weather-info-text">
-        <h2>{data.city}</h2>
-        <p>
-          <FormattedDate date={data.date} />, {data.description}
-        </p>
-        <p>
-          Humidity: <span className="weather-highlight">{data.humidity}%</span>,
-          Wind:{" "}
-          <span className="weather-highlight">
-            {Math.round(data.wind)} {unit === "imperial" ? "mph" : "km/h"}
-          </span>
-        </p>
-      </div>
-      <div className="weather-info-temp">
-        <WeatherIcon
-          code={data.icon.split("/").pop().replace(".png", "")}
-          size={64}
-        />
-        <div className="weather-app-temp">
-          {Math.round(data.temperature)}
-          <span className="weather-app-unit">
-            {unit === "imperial" ? "°F" : "°C"}
-          </span>
+    <div className="WeatherInfo">
+      <div className="info-layout">
+        <div className="left-column">
+          <h1>{data.city}</h1>
+          <p>
+            <FormattedDate date={data.date} />, {data.description}
+            <br />
+            Humidity: <strong className="highlight">{data.humidity}%</strong>,
+            Wind:{" "}
+            <strong className="highlight">
+              {Math.round(data.wind)} {unit === "imperial" ? "mph" : "kph"}
+            </strong>
+          </p>
+        </div>
+        <div className="right-column">
+          <div className="weather-app-temp-row">
+            <WeatherIcon
+              code={data.icon.split("/").pop().replace(".png", "")}
+              size={52}
+            />
+            <div className="weather-app-temp">
+              {Math.round(data.temperature)}
+              <span className="weather-app-unit">
+                {unit === "imperial" ? "°F" : "°C"}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
